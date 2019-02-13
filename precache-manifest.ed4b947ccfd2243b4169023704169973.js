f330b0972e2b1c0404ced3649e9472876648287c
isuruAb <vpisabeywardana@gmail.com>
2019-02-13 13:40:40 +0800

    Updates

---
+++ precache-manifest.ed4b947ccfd2243b4169023704169973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6675a42c2bdd0daf4323",
+    "url": "/testPWA/static/css/main.3b91e06e.chunk.css"
+  },
+  {
+    "revision": "6675a42c2bdd0daf4323",
+    "url": "/testPWA/static/js/main.6675a42c.chunk.js"
+  },
+  {
+    "revision": "b7f197c6c8270e48bfa2",
+    "url": "/testPWA/static/js/runtime~main.b7f197c6.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/testPWA/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testPWA/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "705fbdd6bb24dc1ffca6d7b9cd643c52",
+    "url": "/testPWA/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/testpwa/precache-manifest.6e0f3f6798cd7cbbd8caf005d5e4da9b.js"
+  "/testPWA/precache-manifest.ed4b947ccfd2243b4169023704169973.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/testpwa/index.html", {
+workbox.routing.registerNavigationRoute("/testPWA/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
