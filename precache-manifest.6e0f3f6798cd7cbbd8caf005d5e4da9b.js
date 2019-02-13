45227e8106e7bbecb979a67fb3bba2c2146c7ea7
isuruAb <vpisabeywardana@gmail.com>
2019-02-13 13:35:00 +0800

    Updates

---
+++ precache-manifest.6e0f3f6798cd7cbbd8caf005d5e4da9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ce79e28ab0791b0a2ab",
+    "url": "/testpwa/static/css/main.3b91e06e.chunk.css"
+  },
+  {
+    "revision": "6ce79e28ab0791b0a2ab",
+    "url": "/testpwa/static/js/main.6ce79e28.chunk.js"
+  },
+  {
+    "revision": "06b3014b716ec8bff0e8",
+    "url": "/testpwa/static/js/runtime~main.06b3014b.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/testpwa/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testpwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "08dcfa77e9998617cc872629f935e477",
+    "url": "/testpwa/index.html"
+  }
+];
